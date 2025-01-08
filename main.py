92f88c5df0994b4a33596ff2d94c04a940969580
Cody <chodywhooch@live.ca>
2025-01-08 02:38:25 -0700

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ data_path = f'{application_path}data\\'
 Path(data_path).mkdir(parents=True, exist_ok=True)
 
 time_format = "%Y-%m-%d %H:%M:%S"
-id_streamer = "50014629"  # "268136120"  # Chody  # "50014629"  # Ronin
+id_streamer = "268136120"  # "268136120" - Chody | "50014629" - Ronin
 target_scopes = [AuthScope.CHANNEL_BOT, AuthScope.USER_READ_CHAT, AuthScope.USER_WRITE_CHAT, AuthScope.CHANNEL_MODERATE, AuthScope.USER_READ_BROADCAST, AuthScope.MODERATOR_MANAGE_CHAT_MESSAGES]
 moderators = ("563919062", "659673020", "57231965", "755808445", "742401277", "659640208", "517351918", "195782134", "761241392", "588989623", "781923878", "82006620", "192918528", "872594380", "1211252085")
 
@@ -58,9 +58,9 @@ class WebsocketsManager:
 
 
 def refresh_total_goal():
-    with open(f"{data_path}cash_goal", "r") as file:
+    with open(f"{data_path}cash_goal.txt", "r") as file:
         cash_goal = int(file.read())
-    with open(f"{data_path}cash_total", "r") as file:
+    with open(f"{data_path}cash_total.txt", "r") as file:
         cash_total = int(file.read())
     return cash_goal, cash_total
 
@@ -94,7 +94,7 @@ async def on_stream_message(data: ChannelChatMessageEvent):
                 amount = amount.removeprefix("add")
                 if amount.isdigit():
                     cash_total += int(amount)
-                    with open(f"{data_path}cash_total", "w") as file:
+                    with open(f"{data_path}cash_total.txt", "w") as file:
                         file.write(str(cash_total))
                 else:
                     await bot.send_chat_message(id_streamer, id_streamer, error_msg)
@@ -104,7 +104,7 @@ async def on_stream_message(data: ChannelChatMessageEvent):
                 amount = amount.removeprefix("remove")
                 if amount.isdigit():
                     cash_total -= int(amount)
-                    with open(f"{data_path}cash_total", "w") as file:
+                    with open(f"{data_path}cash_total.txt", "w") as file:
                         file.write(str(cash_total))
                 else:
                     await bot.send_chat_message(id_streamer, id_streamer, error_msg)
@@ -113,8 +113,9 @@ async def on_stream_message(data: ChannelChatMessageEvent):
                 await bot.send_chat_message(id_streamer, id_streamer, error_msg)
                 error = True
             if not error:
+                remaining = int(cash_goal - cash_total)
                 obs.set_text(obs_source_name, f"CashApp Bet\n${cash_total}/{cash_goal}\n$roningt81")
-                await bot.send_chat_message(id_streamer, id_streamer, f"CashApp Bet Update; ${amount} {'contributed' if add else 'removed due to correction'}; ${cash_goal - cash_total} remaining; {time_end - datetime.datetime.strptime(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), time_format)} time left.")
+                await bot.send_chat_message(id_streamer, id_streamer, f"CashApp Bet Update; ${amount} {'contributed' if add else 'removed due to correction'}; ${f'{remaining:,} remaining' if remaining >= 0 else f'{abs(remaining):,} extra for Ronin'}; Time Left: {time_end - datetime.datetime.strptime(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), time_format)}.")
         elif data.event.chatter_user_id in moderators:
             await bot.send_chat_message(id_streamer, id_streamer, f"This command is restricted to RoninGT and TheeChody temporarily")
         else:
@@ -142,7 +143,8 @@ async def run():
                 await shutdown()
             elif option == "1":
                 cash_goal, cash_total = refresh_total_goal()
-                await bot.send_chat_message(id_streamer, id_streamer, f"CashApp Bet Update; ${cash_goal - cash_total} remaining; {time_end - datetime.datetime.strptime(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), time_format)} time left.")
+                remaining = int(cash_goal - cash_total)
+                await bot.send_chat_message(id_streamer, id_streamer, f"CashApp Bet Update; ${f'{remaining:,} remaining' if remaining >= 0 else f'{abs(remaining):,} extra for Ronin'}; Time Left: {time_end - datetime.datetime.strptime(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), time_format)}.")
             elif option == "2":
                 cash_goal, cash_total = refresh_total_goal()
                 try:
@@ -150,10 +152,11 @@ async def run():
                     cash_total += cash_add
                 except Exception as f:
                     print(f"Not Valid, try again -- {f}")
-                with open(f"{data_path}cash_total", "w") as file:
+                with open(f"{data_path}cash_total.txt", "w") as file:
                     file.write(str(cash_total))
+                remaining = int(cash_goal - cash_total)
                 obs.set_text(obs_source_name, f"CashApp Bet\n${cash_total}/{cash_goal}\n$roningt81")
-                await bot.send_chat_message(id_streamer, id_streamer, f"CashApp Bet Update; ${cash_add} contributed to thee CashApp Bet; ${cash_goal - cash_total} remaining. {time_end - datetime.datetime.strptime(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), time_format)} time left.")
+                await bot.send_chat_message(id_streamer, id_streamer, f"CashApp Bet Update; ${cash_add} contributed; ${f'{remaining:,} remaining' if remaining >= 0 else f'{abs(remaining):,} extra for Ronin'}; Time Left: {time_end - datetime.datetime.strptime(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), time_format)}.")
             elif option == "3":
                 cash_goal, cash_total = refresh_total_goal()
                 try:
@@ -161,14 +164,16 @@ async def run():
                     cash_total -= cash_remove
                 except Exception as f:
                     print(f"Not Valid, try again -- {f}")
-                with open(f"{data_path}cash_total", "w") as file:
+                with open(f"{data_path}cash_total.txt", "w") as file:
                     file.write(str(cash_total))
+                remaining = int(cash_goal - cash_total)
                 obs.set_text(obs_source_name, f"CashApp Bet\n${cash_total}/{cash_goal}\n$roningt81")
-                await bot.send_chat_message(id_streamer, id_streamer, f"CashApp Bet Update; ${cash_remove} removed due to correction; ${cash_goal - cash_total} remaining. {time_end - datetime.datetime.strptime(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), time_format)} time left")
+                await bot.send_chat_message(id_streamer, id_streamer, f"CashApp Bet Update; ${cash_remove} removed due to correction; ${f'{remaining:,} remaining' if remaining >= 0 else f'{abs(remaining):,} extra for Ronin'}; Time Left: {time_end - datetime.datetime.strptime(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), time_format)}.")
             else:
                 print("IDK what key you pressed, but that wasn't valid"), time.sleep(5)
         except KeyboardInterrupt:
-            pass
+            print("Exiting program.."), time.sleep(2)
+            await shutdown()
         except Exception as e:
             print(f"Error occurred -- {e}\nProgram will close in 120 seconds, or close program manually"), time.sleep(120)
             await shutdown()
@@ -176,31 +181,31 @@ async def run():
 
 if __name__ == "__main__":
     trigger = ""
-    with open(f"{data_path}twitch_client", "r") as file:
+    with open(f"{data_path}twitch_client.txt", "r") as file:
         twitch_client = file.read()
         if twitch_client == "":
             trigger += "twitch_client\n"
-    with open(f"{data_path}twitch_secret", "r") as file:
+    with open(f"{data_path}twitch_secret.txt", "r") as file:
         twitch_secret = file.read()
         if twitch_secret == "":
             trigger += "twitch_secret\n"
-    with open(f"{data_path}obs_host", "r") as file:
+    with open(f"{data_path}obs_host.txt", "r") as file:
         obs_host = file.read()
         if obs_host == "":
             trigger += "obs_host\n"
-    with open(f"{data_path}obs_port", "r") as file:
+    with open(f"{data_path}obs_port.txt", "r") as file:
         obs_port = file.read()
         if obs_port == "":
             trigger += "obs_port\n"
-    with open(f"{data_path}obs_pass", "r") as file:
+    with open(f"{data_path}obs_pass.txt", "r") as file:
         obs_pass = file.read()
         if obs_pass == "":
             trigger += "obs_pass\n"
-    with open(f"{data_path}obs_scene_name", "r") as file:
+    with open(f"{data_path}obs_scene_name.txt", "r") as file:
         obs_scene_name = file.read()
         if obs_scene_name == "":
             trigger += "obs_scene_name\n"
-    with open(f"{data_path}obs_source_name", "r") as file:
+    with open(f"{data_path}obs_source_name.txt", "r") as file:
         obs_source_name = file.read()
         if obs_source_name == "":
             trigger += "obs_source_name\n"
@@ -227,32 +232,33 @@ if __name__ == "__main__":
             time_start = datetime.datetime.strptime(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), time_format)
             time_end = input("Input end date; YYYY-mm-dd HH:MM:SS\n")
             time_end = datetime.datetime.strptime(time_end, time_format)
-            with open(f"{data_path}time_start", "w") as file_start:
+            with open(f"{data_path}time_start.txt", "w") as file_start:
                 file_start.write(str(time_start))
-            with open(f"{data_path}time_end", "w") as file_end:
+            with open(f"{data_path}time_end.txt", "w") as file_end:
                 file_end.write(str(time_end))
             cash_goal = int(input("Enter CashApp Goal Value\n"))
-            with open(f"{data_path}cash_goal", "w") as file_cash_goal:
+            with open(f"{data_path}cash_goal.txt", "w") as file_cash_goal:
                 file_cash_goal.write(str(cash_goal))
         else:
-            with open(f"{data_path}time_start", "r") as file_start:
+            with open(f"{data_path}time_start.txt", "r") as file_start:
                 time_start = file_start.read()
                 time_start = datetime.datetime.strptime(time_start, time_format)
-            with open(f"{data_path}time_end", "r") as file_end:
+            with open(f"{data_path}time_end.txt", "r") as file_end:
                 time_end = file_end.read()
                 time_end = datetime.datetime.strptime(time_end, time_format)
-            with open(f"{data_path}cash_goal", "r") as file_cash_goal:
+            with open(f"{data_path}cash_goal.txt", "r") as file_cash_goal:
                 cash_goal = int(file_cash_goal.read())
-            with open(f"{data_path}cash_total", "r") as file_cash_total:
+            with open(f"{data_path}cash_total.txt", "r") as file_cash_total:
                 cash_total = int(file_cash_total.read())
 
-        print(f"{'Initialization' if initialize else 'Files Loaded'} successful!\nTime Start; {time_start}\nTime Till End; {time_end - datetime.datetime.strptime(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), time_format)}")
+        print(f"{'Initialization' if initialize else 'Files Loaded'} successful{'ly' if not initialize else ''}!\nTime Start; {time_start}\nTime Till End; {time_end - datetime.datetime.strptime(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), time_format)}")
 
         obs.set_source_visibility(obs_scene_name, obs_source_name, True)
         obs.set_text(obs_source_name, f"CashApp Bet\n${cash_total}/{cash_goal}\n$roningt81")
 
     except KeyboardInterrupt:
-        pass
+        print("Exiting program.."), time.sleep(2)
+        os._exit(1)
     except Exception as e:
         print(f"Error occurred -- {e}\nProgram will close in 120 seconds, or close program manually"), time.sleep(120)
         os._exit(1)
